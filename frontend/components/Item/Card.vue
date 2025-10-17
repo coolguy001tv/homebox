db8f301212a7b09604153c9387f3d4114af848e9
CoolGuy <cg001@live.com>
2025-10-17 09:57:14 +0800

    优化样式，始终保持4/3的图片

--- frontend/components/Item/Card.vue
+++ frontend/components/Item/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink class="group card rounded-md border border-gray-300" :to="`/item/${item.id}`">
-    <div class="relative h-[200px]">
-      <img v-if="imageUrl" class="h-[200px] w-full object-cover rounded-t shadow-sm border-gray-300" :src="imageUrl" />
+    <div class="relative">
+      <img v-if="imageUrl" class="w-full aspect-[4/3] object-cover rounded-t shadow-sm border-gray-300" :src="imageUrl" />
       <div class="absolute bottom-1 left-1">
         <NuxtLink
           v-if="item.location"
